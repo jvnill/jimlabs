8de4eab1aab06008b187c2f8ff108c09b1649ccf
Jim Ruther Nill <jvnill@gmail.com>
2014-08-07 02:41:25 +0800

    update routes and portfolio page

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,23 +1,18 @@
 class Portfolio < ActiveRecord::Base
   acts_as_taggable_on :tags
-  acts_as_commentable
 
-  # VALIDATIONS
   validates_presence_of :url, :title, :path, :body
 
-  # NAMED SCOPE
   scope :published, where(:published => true)
 
-  # CALLBACKS
   before_validation :set_path
 
-  # INSTANCE METHODS
   def photo_url(size = :original)
     "/images/#{path.downcase}_#{size.to_s}.png"
   end
 
   def meta_tags
-    {:keywords => tag_list, :description => meta}
+    { keywords: tag_list, description: meta}
   end
 
   def to_param
@@ -28,7 +23,6 @@ class Portfolio < ActiveRecord::Base
     self.path = title.downcase.gsub(/[^A-Za-z0-9]+/, '-') if path.blank?
   end
 
-  # CLASS METHODS
   def self.meta_tags
     published.collect(&:tag_list).flatten.uniq.join(',')
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Jimlabs3::Application.routes.draw do
+  root to: "pages#index"
 
-  resources :enquiries, :except => [:update, :edit, :show]
-  resources :portfolios
-  resources :posts
-  resources :comments, :only => [:create, :index, :update]
-
-  match '/blog/rss/feed' => 'posts#index', :constraints => {:format => 'rss'}
-  match '/blog/:id/rss/feed' => 'posts#show', :constraints => {:format => 'rss'}
-
-  root :to => "pages#index"
   resource :session
 
-  match '/contact'           => "enquiries#new",    :as => :contact
-  match '/admin'             => "sessions#new",     :as => :admin
-  match '/logout'            => "sessions#destroy", :as => :logout
-  match '/tags/:tag_name'    => "tags#show",        :as => :tag
-  match '/archives/:date'    => "archives#show",    :as => :archive
-  match '/sitemap(.:format)' => "pages#sitemap",    :as => :sitemap, :defaults => {:format => :xml}
-  match '*url'               => "pages#page_not_found"
+  resources :enquiries, only: [:index, :new, :create, :destroy]
+  resources :posts
+  resources :comments, only: [:create, :index, :update]
+
+  match '/portfolio'         => 'portfolios#index', as: :portfolio
+  match '/contact'           => 'enquiries#new',    as: :contact
+  match '/admin'             => 'sessions#new',     as: :admin
+  match '/logout'            => 'sessions#destroy', as: :logout
+  match '/tags/:tag_name'    => 'tags#show',        as: :tag
+  match '/archives/:date'    => 'archives#show',    as: :archive
+  match '/sitemap(.:format)' => 'pages#sitemap',    as: :sitemap, defaults: { format: :xml }
+
+  match '/blog/rss/feed'     => 'posts#index', constraints: { format: 'rss' }
+  match '/blog/:id/rss/feed' => 'posts#show',  constraints: { format: 'rss' }
+  match '*url'               => 'pages#page_not_found'
 end
