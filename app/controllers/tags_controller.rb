70bdb9c1c5ae7b2695e5edd34c0604e117de07e9
Jim Ruther Nill <jvnill@gmail.com>
2014-08-21 23:05:32 +0800

    fix tags page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
-  skip_before_filter :login_required
+  skip_before_action :login_required
+
   def show
-    @posts = Post.tagged_with params[:tag_name]
-    @portfolios = Portfolio.tagged_with params[:tag_name]
+    @posts = Post.tagged_with(params[:tag_name])
   end
 end
